6bf0a0660b11d5fcebb31e34d44294876b511696
Your Name <you@example.com>
2021-02-12 09:22:33 +0100

    fixed upper case

--- modules/alphabet/alphabet.py
+++ modules/alphabet/alphabet.py
@@ -4,9 +4,9 @@ def alphabet_gen(alphabet_type):
     if alphabet_type == 'az':
         return [chr(i) for i in range(97, 123)]
     if alphabet_type == 'aZ':
-        return alphabet_gen('az') + alphabet_gen('az').upper()
+        return alphabet_gen('az') + alphabet_gen('AZ')
     if alphabet_type == 'AZ':
-        return alphabet_gen('az').upper()
+        return [letter.upper() for letter in alphabet_gen('az')]
     if alphabet_type is None:
         return alphabet_gen('az')
     return [letter for letter in alphabet_type]
